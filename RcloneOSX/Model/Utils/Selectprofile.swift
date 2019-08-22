2bb82471d09e5b332ed109014367c63b5ab7e651
Thomas Evensen <thomeven@gmail.com>
2019-08-22 18:56:54 +0200

    Refactor and cleanup

--- RcloneOSX/Model/Utils/Selectprofile.swift
+++ RcloneOSX/Model/Utils/Selectprofile.swift
@@ -12,19 +12,20 @@ final class Selectprofile {
     var profile: String?
     weak var newProfileDelegate: NewProfile?
     weak var copyfilesnewProfileDelegate: NewProfile?
-    weak var reloadloggdataDelegate: ReadLoggdata?
+    weak var loggdataProfileDelegate: NewProfile?
 
     init(profile: String?) {
         self.profile = profile
         self.newProfileDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vctabmain) as? ViewControllertabMain
         self.copyfilesnewProfileDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vccopyfiles) as? ViewControllerCopyFiles
+        self.loggdataProfileDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vcloggdata) as? ViewControllerLoggData
         if self.profile == "Default profile" {
             newProfileDelegate?.newProfile(profile: nil)
         } else {
             newProfileDelegate?.newProfile(profile: self.profile)
         }
         self.copyfilesnewProfileDelegate?.newProfile(profile: nil)
-        self.reloadloggdataDelegate?.readloggdata()
+        self.loggdataProfileDelegate?.newProfile(profile: nil)
     }
 
 }
