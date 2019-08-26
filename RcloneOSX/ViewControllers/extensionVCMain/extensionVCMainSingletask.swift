710b583379da36d155b8ee61047f50fcfc500f69
Thomas Evensen <thomeven@gmail.com>
2019-08-26 13:30:48 +0200

    Cleanup

---
+++ RcloneOSX/ViewControllers/extensionVCMain/extensionVCMainSingletask.swift
@@ -0,0 +1,66 @@
+//
+//  extensionVCMainSingletask.swift
+//  rcloneosx
+//
+//  Created by Thomas Evensen on 26/08/2019.
+//  Copyright © 2019 Thomas Evensen. All rights reserved.
+//
+//  swiftlint:disable line_length
+
+import Foundation
+import Cocoa
+
+extension ViewControllerMain: SingleTaskProgress {
+
+    func getProcessReference(process: Process) {
+        self.process = process
+    }
+
+    func presentViewProgress() {
+        globalMainQueue.async(execute: { () -> Void in
+            self.presentAsSheet(self.viewControllerProgress!)
+        })
+    }
+
+    func presentViewInformation(outputprocess: OutputProcess) {
+        self.outputprocess = outputprocess
+        if self.dynamicappend {
+            globalMainQueue.async(execute: { () -> Void in
+                self.mainTableView.reloadData()
+            })
+        } else {
+            globalMainQueue.async(execute: { () -> Void in
+                self.presentAsSheet(self.viewControllerInformation!)
+            })
+        }
+    }
+
+    func terminateProgressProcess() {
+        weak var localprocessupdateDelegate: UpdateProgress?
+        localprocessupdateDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vcprogressview) as? ViewControllerProgressProcess
+        localprocessupdateDelegate?.processTermination()
+    }
+
+    func seterrorinfo(info: String) {
+        guard info != "" else {
+            self.dryRunOrRealRun.isHidden = true
+            return
+        }
+        self.dryRunOrRealRun.textColor = setcolor(nsviewcontroller: self, color: .red)
+        self.dryRunOrRealRun.isHidden = false
+        self.dryRunOrRealRun.stringValue = info
+    }
+
+    // Function for getting numbers out of output object updated when
+    // Process object executes the job.
+    func setNumbers(outputprocess: OutputProcess?) {
+        globalMainQueue.async(execute: { () -> Void in
+            guard outputprocess != nil else {
+                self.totalNumber.stringValue = ""
+                return
+            }
+            let number = Numbers(outputprocess: outputprocess)
+            self.totalNumber.stringValue = number.stats()
+        })
+    }
+}
---
+++ RcloneOSX/ViewControllers/extensionVCMain/extensionVCMaintableviewDelegate.swift
@@ -0,0 +1,58 @@
+//
+//  extensionVCMaintableviewDelegate.swift
+//  rcloneosx
+//
+//  Created by Thomas Evensen on 26/08/2019.
+//  Copyright © 2019 Thomas Evensen. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+extension ViewControllerMain: NSTableViewDataSource {
+    // Delegate for size of table
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return self.configurations?.configurationsDataSourcecount() ?? 0
+    }
+}
+
+extension ViewControllerMain: NSTableViewDelegate, Attributedestring {
+    
+    // TableView delegates
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        if row > self.configurations!.configurationsDataSourcecount() - 1 { return nil }
+        let object: NSDictionary = self.configurations!.getConfigurationsDataSource()![row]
+        let markdays: Bool = self.configurations!.getConfigurations()[row].markdays
+        let celltext = object[tableColumn!.identifier] as? String
+        if tableColumn!.identifier.rawValue == "daysID" {
+            if markdays {
+                return self.attributedstring(str: celltext!, color: NSColor.red, align: .right)
+            } else {
+                return object[tableColumn!.identifier] as? String
+            }
+        } else if tableColumn!.identifier.rawValue == "statCellID" {
+            if row == self.index {
+                if self.singletask == nil {
+                    return #imageLiteral(resourceName: "yellow")
+                } else {
+                    return #imageLiteral(resourceName: "green")
+                }
+            }
+        } else {
+            if tableColumn!.identifier.rawValue == "batchCellID" {
+                return object[tableColumn!.identifier] as? Int
+            } else {
+                return object[tableColumn!.identifier] as? String
+            }
+        }
+        return nil
+    }
+    
+    // Toggling batch
+    func tableView(_ tableView: NSTableView, setObjectValue object: Any?, for tableColumn: NSTableColumn?, row: Int) {
+        if self.process != nil {
+            self.abortOperations()
+        }
+        self.configurations!.enabledisablebatch(row)
+    }
+}
