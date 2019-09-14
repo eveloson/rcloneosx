ae19bd38aeef39d323f7effbf8c2e8751bb454da
Thomas Evensen <thomeven@gmail.com>
2019-09-14 07:06:39 +0200

    Updates

--- RcloneOSX/Controller/Configurations/Configurations.swift
+++ RcloneOSX/Controller/Configurations/Configurations.swift
@@ -238,7 +238,7 @@ class Configurations: ReloadTable, SetSchedules {
     }
 
     func getResourceConfiguration(_ hiddenID: Int, resource: ResourceInConfiguration) -> String {
-        var result = self.configurations!.filter({return ($0.hiddenID == hiddenID)})
+        let result = self.configurations!.filter({return ($0.hiddenID == hiddenID)})
         guard result.count > 0 else { return "" }
         switch resource {
         case .localCatalog:
@@ -281,7 +281,7 @@ class Configurations: ReloadTable, SetSchedules {
     /// - parameter none: none
     private func readconfigurations() {
         self.argumentAllConfigurations = [ArgumentsOneConfiguration]()
-        var store: [Configuration]? = self.storageapi!.getConfigurations()
+        let store: [Configuration]? = self.storageapi!.getConfigurations()
         guard store != nil else { return }
         for i in 0 ..< store!.count {
             self.configurations!.append(store![i])
