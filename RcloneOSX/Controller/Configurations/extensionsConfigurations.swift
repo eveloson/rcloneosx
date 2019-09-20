2611329b2439a988fe03c978a9f01198704f16f3
Thomas Evensen <thomeven@gmail.com>
2019-09-20 20:54:02 +0200

    Updates

--- RcloneOSX/Controller/Configurations/extensionsConfigurations.swift
+++ RcloneOSX/Controller/Configurations/extensionsConfigurations.swift
@@ -44,6 +44,7 @@ extension ReloadTable {
     var reloadDelegateMain: Reloadandrefresh? {
         return ViewControllerReference.shared.getvcref(viewcontroller: .vctabmain) as? ViewControllerMain
     }
+
     var reloadDelegateLogData: Reloadandrefresh? {
         return ViewControllerReference.shared.getvcref(viewcontroller: .vcloggdata) as? ViewControllerLoggData
     }
