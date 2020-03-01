8550da738a2248a681601bad000581d0c9bccbca
Thomas Evensen <thomeven@gmail.com>
2020-03-01 18:15:28 +0100

    update

--- RcloneOSX/Model/Execution/Estimating/EstimateremoteInformationOnetask.swift
+++ RcloneOSX/Model/Execution/Estimating/EstimateremoteInformationOnetask.swift
@@ -12,7 +12,7 @@ final class EstimateremoteInformationOnetask: SetConfigurations {
     init(index: Int, outputprocess: OutputProcess?, updateprogress: UpdateProgress) {
         let taskDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vctabmain) as? ViewControllerMain
         let outDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vctabmain) as? ViewControllerMain
-        let arguments = self.configurations!.arguments4rclone(index: index, argtype: .argdryRun)
+        let arguments = self.configurations!.arguments4rclone(index: index, argtype: .argdryrun)
         let process = Rclone(arguments: arguments)
         process.setdelegate(object: updateprogress)
         process.executeProcess(outputprocess: outputprocess)
