d1783754a4958c7069bfea45adbc750b607f6aab
Thomas Evensen <thomeven@gmail.com>
2020-03-07 12:18:22 +0100

    update

--- RcloneOSX/Model/Process/RestoreTask.swift
+++ RcloneOSX/Model/Process/RestoreTask.swift
@@ -10,7 +10,7 @@ import Foundation
 
 final class RestoreTask: SetConfigurations {
     var arguments: [String]?
-    init(index: Int, outputprocess: OutputProcess?, dryrun: Bool, updateprogress: UpdateProgress?) {
+    init(index: Int, outputprocess _: OutputProcess?, dryrun: Bool, updateprogress: UpdateProgress?) {
         weak var setprocessDelegate: SendProcessreference?
         setprocessDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vctabmain) as? ViewControllerMain
         if dryrun {
@@ -28,7 +28,8 @@ final class RestoreTask: SetConfigurations {
         }
         let process = Rclone(arguments: self.arguments)
         process.setdelegate(object: updateprogress!)
-        process.executeProcess(outputprocess: outputprocess)
+        // process.executeProcess(outputprocess: outputprocess)
         setprocessDelegate?.sendprocessreference(process: process.getProcess()!)
+        print(arguments!)
     }
 }
